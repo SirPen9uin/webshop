7902a6ae837af43c9ad963c1570fce87eeddf24b
Dmitrii Kovalev <sirpen9uin@gmail.com>
2024-02-08 18:36:15 +0300

    Админка добавлена

--- shop/shopping/views.py
+++ shop/shopping/views.py
@@ -1,9 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Product
 
 # Create your views here.
 
 
 def index(request):
     template = 'index.html'
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    if request.method == 'POST':
+        pass
     return render(request, template)
-
